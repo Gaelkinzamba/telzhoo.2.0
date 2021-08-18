90369a158d1637ce45ec7af3daa034d9f500f88a
geal tobele kinzamba <81613508+Gaelkinzamba@users.noreply.github.com>
2021-08-18 12:34:39 +0200

    Add files via upload

---
+++ src/utils/newsApi.js
@@ -0,0 +1,71 @@
+export const newsApi =  [
+    {
+        source: {
+            "id": null,
+            "name": "Defense One"
+        },
+        "author": "Thomas Corbett, Peter W. Singer",
+        "title": "China's Fusion Research Is Heating Up - Defense One",
+        "description": "The EAST reactor in Hefei broke records last month as it edges toward the sustained stellar temperatures needed to generate fusion energy.",
+        "url": "https://www.defenseone.com/ideas/2021/06/chinas-fusion-research-heating/174990/",
+        "urlToImage": "https://cdn.defenseone.com/media/img/cd/2021/06/28/EAST_plasma_geometry_and_3D_view.webp-2/open-graph.png",
+        "publishedAt": "2021-06-28T13:45:17Z",
+        "content": "The hottest thing in the solar system is usually the Sun, with its core temperature of 15 million degrees Celsius. But for nearly two minutes on May 28, the title was held by an experimental fusion r… [+4622 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Digital Trends"
+        },
+        "author": "Monica J. White",
+        "title": "New leak suggests that AMD is about to release two new graphics cards",
+        "description": "According to a leak, AMD is about to release two new graphics cards. AMD Radeon RX 6600 and RX 6600XT both house a Navi 23 GPU and may hit the market soon.",
+        "url": "https://www.digitaltrends.com/computing/new-amd-radeon-graphics-cards-launching-soon/",
+        "urlToImage": "https://icdn.digitaltrends.com/image/digitaltrends/amd-radeon-rx-6000-series-graphics-card_big-navi-scaled-2.jpg",
+        "publishedAt": "2021-06-28T17:19:32Z",
+        "content": "AMD may be about to launch two new Navi 23 graphics cards, according to a new leak. Drivers for the RX 6600 and RX 6600XT have been found in a recent update, suggesting that AMD is likely preparing t… [+2603 chars]"
+    
+    },
+    {
+        source: {
+            "id": "the-wall-street-journal",
+            "name": "The Wall Street Journal"
+        },
+        "author": "Eric Morath, Joe Barrett",
+        "title": "Americans Leaving Unemployment Rolls More Quickly in States Cutting Off Benefits...",
+        "description": "Americans Leaving Unemployment Rolls More Quickly in States Cutting Off Benefits...\r\n\n \n \n \n (Second column, 15th story, link)\r\n\n \r\n\n \r\n\n \n Related stories:ROARING 20S: Vegas Home Sells for Record $25 Million...\r\nIPO Market Never Been Hotter...\r\nHaters Everyw…",
+        "url": "https://www.wsj.com/articles/americans-are-leaving-unemployment-rolls-more-quickly-in-states-cutting-off-benefits-11624786202",
+        "urlToImage": "https://images.wsj.net/im-360633/social",
+        "publishedAt": "2021-06-27T12:22:37Z",
+        "content": "The number of unemployment-benefit recipients is falling at a faster rate in Missouri and 21 others states canceling enhanced and extended payments this month, suggesting that ending the aid could pu… [+6872 chars]"
+    
+    },
+    {
+        source: {
+            "id": "the-wall-street-journal",
+            "name": "The Wall Street Journal"
+        },
+        "author": "chetan",
+        "title": "Natural-Pool Owners Are Kissing Chlorine and Other Chemicals Goodbye - WSJ",
+        "description": "Intense demand during Covid and a shortage of chlorine tabs have homeowners hopping from chemical-treated pools to greener alternatives",
+        "url": "https://www.wsj.com/articles/natural-pools-green-alternative-chlorine-11624472961",
+        "urlToImage": "https://images.wsj.net/im-356344/social",
+        "publishedAt": "2021-06-28T05:47:02Z",
+        "content": "This summer, some homeowners will be swimming in their backyard pools au naturel. The iconic, aqua-blue vessels of chlorine-treated water are starting to see competition from the leafy, greenish wate… [+1203 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Jalopnik"
+        },
+        "author": "Mercedes Streeter",
+        "title": "The Aston Martin Valkyrie AMR Pro Ditches Hybrid As It Gets Freed From Le Mans Car Red Tape - Jalopnik",
+        "description": "This track-only non-hybrid is what you get when a manufacturer builds a car without any rules.",
+        "url": "https://jalopnik.com/the-aston-martin-valkyrie-amr-pro-ditches-hybrid-as-it-1847185082",
+        "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/9010a0430c1948b1e550578ddc4b348b.jpg",
+        "publishedAt": "2021-06-28T15:16:00Z",
+        "content": "Aston Martins bid to compete in the Le Mans Hypercar class may have been shelved, but its plan to build an extreme track weapon is not. Unburdened from road and Le Mans rules, the updated Aston Marti… [+2283 chars]"
+    
+    },
+
+]
