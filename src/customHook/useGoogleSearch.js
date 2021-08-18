4ab31decaa733a8992eaa5754b805ad4fda44e82
geal tobele kinzamba <81613508+Gaelkinzamba@users.noreply.github.com>
2021-08-18 12:33:13 +0200

    Add files via upload

---
+++ src/customHook/useGoogleSearch.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import API_KEY from '../keys';
+
+const CONTEXT_KEY = "2add8e93f8fe55f09";
+
+const useGoogleSearch = (term, startIndex) => {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchDate = async() => {
+            fetch(
+                    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}&start=${startIndex}`
+                )
+                .then(res => res.json())
+                .then(result => {
+                    setData(result)
+                })
+        }
+
+        fetchDate();
+    }, [term, startIndex])
+
+
+    return { data }
+}
+
+export default useGoogleSearch
